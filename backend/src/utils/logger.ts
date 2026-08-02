739c14ae5c175afcfae460857c121c8371680b6b
sankeerth06 <nsaisankeerth@gmail.com>
2026-08-02 21:42:21 +0530

    Update logger.ts

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,5 +1,4 @@
 import winston from 'winston';
-import path from 'path';
 
 const { combine, timestamp, printf, colorize, errors } = winston.format;
 
@@ -13,13 +12,6 @@ export const logger = winston.createLogger({
   transports: [
     new winston.transports.Console({
       format: combine(colorize(), timestamp(), logFormat),
-    }),
-    new winston.transports.File({
-      filename: path.join(process.cwd(), 'logs', 'error.log'),
-      level: 'error',
-    }),
-    new winston.transports.File({
-      filename: path.join(process.cwd(), 'logs', 'combined.log'),
     }),
   ],
   exitOnError: false,
